48d645b2d3aa9f0175230cb9fce97dad7f728e45
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-17 15:17:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+
 
 function App() {
+  // let name='mehak';
+  // let l=[12,13,23,24,34,45,46];
+  // let obj={
+  //   'name':'cvb',
+  //   'cname':'dftghju',
+  //   'desc':'asdfgyujk'
+  // }
+  // let status=true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main">
+      {/* <h1>{name} hiiii</h1>
+      {l.map((v)=>{ //direct data render krne k lie
+        return (
+          <h2>{v}</h2>
+        )
+      })}
+      <div>{obj.name} </div>
+      <div>{obj.cname} </div>
+      <div>{obj.desc} </div>
+      { (status)  ? 
+        <div style={{color:'red',backgroundColor:'yellow'}}>welcome to ws</div>
+      : ""} */ }
+      
+      <Header/>
+          <div className="row">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      <Footer/>
+      </div>
+      
   );
 }
 
 export default App;
+
+
+function Card(){
+  return(
+    <div className='carditems'>Card Div</div>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>Header
+        <div>hii</div>
+        <div>hiii</div>
+        <div>hiiii</div>
+    
+    </div>
+  )
+}
